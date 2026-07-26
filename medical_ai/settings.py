c486a33573d07d4d615e1808a81a286214d55731
M Srinivas <srinivas.yadav18082002@gmail.com>
2026-07-26 22:54:46 +0530

    Configure project for Render deployment

--- medical_ai/settings.py
+++ medical_ai/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-!+&x*3iguaw!&_el1d9l-!zp%qtsvpa(^6i__ax)a1)_50^0l9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,10 +135,12 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
